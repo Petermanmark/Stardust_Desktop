42f6ce5f5ed26ac0ccbe3eb603b21b536042e370
Petermanmark <petermanmark12@gmail.com>
2023-04-18 00:38:51 +0200

    card

--- Test/MainWindow.xaml.cs
+++ Test/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using AdaptiveCards;
+using AdaptiveCards.Rendering;
+using AdaptiveCards.Rendering.Wpf;
+using Microsoft.AspNetCore.Components.RenderTree;
+using Newtonsoft.Json;
+using OxyPlot;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +29,41 @@ namespace Test
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Create the JSON content for the Adaptive Card
+            var cardJson = @"
+            {
+                ""$schema"": ""http://adaptivecards.io/schemas/adaptive-card.json"",
+                ""type"": ""AdaptiveCard"",
+                ""version"": ""1.0"",
+                ""body"": [
+                    {
+                        ""type"": ""TextBlock"",
+                        ""text"": ""Title of the card"",
+                        ""weight"": ""bolder"",
+                        ""size"": ""medium""
+                    },
+                    {
+                        ""type"": ""TextBlock"",
+                        ""text"": ""Content of the card"",
+                        ""wrap"": true
+                    }
+                ]
+            }";
+
+            // Parse the JSON content into an AdaptiveCard object
+            var card = AdaptiveCard.FromJson(cardJson).Card;
+
+            // Render the Adaptive Card into a WPF container
+            var renderer = new AdaptiveCardRenderer();
+            var uiElement = renderer.RenderCard(card).FrameworkElement;
+
+            // Add the Adaptive Card to the window
+            MainGrid.Children.Add(uiElement);
         }
 
     }
