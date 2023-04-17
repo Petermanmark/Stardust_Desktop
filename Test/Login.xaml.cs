9e1e2d8a2e1c4794688b8dacc2e5168c9c5edb36
hyimred <ptrk.csak@gmail.com>
2023-04-17 23:06:48 +0200

    login

--- Test/Login.xaml.cs
+++ Test/Login.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,25 +27,86 @@ namespace Test
             InitializeComponent();
         }
 
-        private void OnLoginButtonClicked(object sender, RoutedEventArgs e)
+        private async void OnLoginButtonClicked(object sender, RoutedEventArgs e)
         {
             // Get the entered username and password
-            string username = UsernameTextBox.Text.ToString();
+            string email = EmailTextBox.Text.ToString();
             string password = PasswordBox.Password.ToString();
 
-            // Perform the login check (replace with your own authentication logic)
-            bool isLoginSuccessful = false; // replace this with your own authentication logic
-            if (isLoginSuccessful)
+            if (string.IsNullOrEmpty(email) || !IsValidEmail(email))
             {
-                // Redirect the user to the main window
+                MessageBox.Show("Please enter a valid email address");
+                return;
+            }
+            if (string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please enter a password");
+                return;
+            }
+
+            try
+            {
+                string result = await PerformUserLogin(email, password);
+
+                // Display a success message
+                MessageBox.Show(result);
+
+                // Redirect the user to the login window
                 MainWindow mainWindow = new MainWindow();
                 mainWindow.Show();
                 Close();
             }
-            else
+            catch (Exception ex)
             {
                 // Display an error message
-                ErrorMessageTextBlock.Text = "Invalid username or password.";
+                MessageBox.Show(ex.Message);
+            }           
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private async Task<string> PerformUserLogin(string email, string password)
+        {
+            // Create an HTTP client object
+            using (HttpClient client = new HttpClient())
+            {
+                // Set the base URL for the API
+                client.BaseAddress = new Uri("http://localhost:3000");
+
+                // Create a JSON object with the username, email, and password
+                UserRegister user = new UserRegister();
+                user.email = email;
+                user.password = password;
+                var json = JsonConvert.SerializeObject(user);
+                Trace.WriteLine(user);
+
+                // Create an HTTP request message with the JSON object in the body
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = await client.PostAsync("/auth/login", content);
+
+                // If the response was successful, return the success message
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    return result;
+                }
+                else
+                {
+                    // If the response was not successful, throw an exception with the error message
+                    var error = await response.Content.ReadAsStringAsync();
+                    throw new Exception(error);
+                }
             }
         }
     }
