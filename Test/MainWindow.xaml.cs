c4e62e289958fe0adc8781906478f1364c48205b
Petermanmark <petermanmark12@gmail.com>
2023-04-18 21:51:30 +0200

    Edit, Delete buttons

--- Test/MainWindow.xaml.cs
+++ Test/MainWindow.xaml.cs
@@ -31,8 +31,9 @@ namespace Test
         public MainWindow()
         {
             InitializeComponent();
+            //this.WindowState = WindowState.Maximized;
+            this.ResizeMode = ResizeMode.NoResize;
             getNotes(stackPanel);
-
         }
 
         static async void getNotes(StackPanel stackPanel)
@@ -52,7 +53,7 @@ namespace Test
                     dynamic data = JsonConvert.DeserializeObject(json);
 
                     // Define the stack panel that will contain the cards
-                    stackPanel.Margin = new Thickness(10);
+                    stackPanel.Margin = new Thickness(10);;
 
                     // Assume that the API data is an array of objects with "name" property
                     foreach (dynamic item in data)
@@ -65,19 +66,19 @@ namespace Test
 
                         // Create the card
                         Border card = new Border();
-                        card.MaxWidth = 800;
                         card.BorderBrush = System.Windows.Media.Brushes.Black;
                         card.BorderThickness = new Thickness(1);
                         card.Margin = new Thickness(5);
+                        card.Padding = new Thickness(15);
 
                         Grid gridCard = new Grid();
                         gridCard.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Auto });
                         gridCard.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+                        gridCard.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Auto });
 
                         TextBlock titleCard = new TextBlock();
                         titleCard.Text = title;
-                        titleCard.FontSize = 30;
-                        titleCard.MaxWidth = 500;
+                        titleCard.FontSize = 20;
                         titleCard.FontWeight = System.Windows.FontWeights.Bold;
                         titleCard.Margin = new Thickness(5);
                         titleCard.TextWrapping = TextWrapping.Wrap;
@@ -86,12 +87,15 @@ namespace Test
 
                         TextBlock contentCard = new TextBlock();
                         contentCard.Text = content;
-                        contentCard.FontSize = 20;
-                        contentCard.MaxWidth = 500;
+                        contentCard.FontSize = 12;
                         contentCard.Margin = new Thickness(5);
                         contentCard.TextWrapping = TextWrapping.Wrap;
                         Grid.SetRow(contentCard, 1);
                         Grid.SetColumn(contentCard, 1);
+
+                        // Bind the TextBlocks' Width property to the parent element's ActualWidth
+                        titleCard.SetBinding(TextBlock.WidthProperty, new Binding("ActualWidth") { Source = card });
+                        contentCard.SetBinding(TextBlock.WidthProperty, new Binding("ActualWidth") { Source = card });
 
                         // Wrap the TextBlocks in a Viewbox to make them responsive
                         Viewbox titleViewbox = new Viewbox();
@@ -108,6 +112,26 @@ namespace Test
                         Grid.SetRow(contentViewbox, 1);
                         Grid.SetColumn(contentViewbox, 1);
                         gridCard.Children.Add(contentViewbox);
+
+                        // Create a StackPanel for the buttons
+                        StackPanel buttonPanel = new StackPanel();
+                        buttonPanel.Orientation = Orientation.Horizontal;
+                        buttonPanel.Margin = new Thickness(5);
+                        Grid.SetRow(buttonPanel, 2);
+                        Grid.SetColumn(buttonPanel, 1);
+
+                        // Create the buttons and add them to the StackPanel
+                        Button editButton = new Button();
+                        editButton.Content = "Edit";
+                        editButton.Margin = new Thickness(5);
+                        buttonPanel.Children.Add(editButton);
+
+                        Button deleteButton = new Button();
+                        deleteButton.Content = "Delete";
+                        deleteButton.Margin = new Thickness(5);
+                        buttonPanel.Children.Add(deleteButton);
+
+                        gridCard.Children.Add(buttonPanel);
 
                         card.Child = gridCard;
                         stackPanel.Children.Add(card);
